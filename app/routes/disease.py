a890fbc5bb892a57c4dd0f56e11ac9068b526aba
Daprogammer <dhairyashah0409@gmail.com>
2026-09-13 14:23:53 +0530

    - work on disease paused the work due to being no ui for image upload

--- app/routes/disease.py
+++ app/routes/disease.py
@@ -1,11 +1,18 @@
 """Mandatory core API. Connect trained inference here when available."""
-from flask import Blueprint
+import os.path
+
+from flask import Blueprint,request
 
 from .responses import not_implemented
+from model.predict import predict as get_prediction
+import config
 
 bp = Blueprint("disease", __name__)
 
 
+# TODO: Create a seperate ui for image upload or keep it as a SPA by adding upload ui in index.html
 @bp.post("/api/disease/predict")
 def predict():
+    # prediction = get_prediction(config.Config.UPLOAD_FOLDER)
+    # _,extension = os.path.split(request.files[])
     return not_implemented("disease")
